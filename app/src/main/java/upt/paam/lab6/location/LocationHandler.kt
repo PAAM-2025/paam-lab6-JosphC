bbee31b6c4c34035d2c0792b23f940b79fa45a2f
JosphC <master.luck005@gmail.com>
2025-12-08 02:08:38 +0200

    Done

--- app/src/main/java/upt/paam/lab6/location/LocationHandler.kt
+++ app/src/main/java/upt/paam/lab6/location/LocationHandler.kt
@@ -16,11 +16,18 @@ class LocationHandler(context: Context) {
         LocationServices.getFusedLocationProviderClient(context)
 
     fun registerLocationListener(locationCallback: LocationCallback) {
-        // TODO 2: Create a LocationRequest with Priority.PRIORITY_HIGH_ACCURACY,
-        //  min update distance set to 5m and update interval to 100ms.
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            100
+        ).apply {
+            setMinUpdateDistanceMeters(5f)
+        }.build()
 
-        // TODO 3: Register request and callback with the fused location service client.
-
+        client.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
     }
 
     fun unregisterLocationListener(locationCallback: LocationCallback) {
